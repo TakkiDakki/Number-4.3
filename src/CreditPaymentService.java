acbb16a50fed10972ceee0c4211ef73b1daaaa18
Yulia Kobeleva <kobelevaulia@gmail.com>
2021-04-04 22:32:49 +0300

    commt

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,5 @@
 public class CreditPaymentService {
     public float calculate(int credit, int paymentyear, double creditpercent) {
-        int year;
-        paymentyear = year * 12;
-
 
 
         long monthpayment = (long) ((long) credit * (creditpercent / (1 + creditpercent) - paymentyear - 1));
